1cdbfa45a45dc33c376c5c8a2fa63a978d46dff3
Miguel Vitta <miguelvittadev@gmail.com>
2026-03-20 10:05:44 -0300

    feat: adds a preprocessing step of lowercasing the query and the results to avoid problems with casing

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -2,11 +2,18 @@ from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies
 
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
-    movies_dict = load_movies()
+    movies = load_movies()
     results = []
-    for movie in movies_dict:
-        if query in movie["title"]:
+    for movie in movies:
+        preprocessed_query = preprocess_text(query)
+        preprocessed_title = preprocess_text(movie["title"])
+        if preprocessed_query in preprocessed_title:
             results.append(movie)
             if len(results) >= limit:
                 break
     return results
+
+
+def preprocess_text(text: str) -> str:
+    text = text.lower()
+    return text
