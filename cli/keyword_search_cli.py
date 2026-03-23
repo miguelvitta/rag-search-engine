a05c7342ce22df4ded7efb3c0901a197e45ae9f4
Miguel Vitta <miguelvittadev@gmail.com>
2026-03-23 16:59:00 -0300

    feat: adds the frequence counter calling

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-from lib.keyword_search import build_command, search_command
+from lib.keyword_search import build_command, search_command, tf_command
 
 
 def main() -> None:
@@ -14,6 +14,12 @@ def main() -> None:
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
+    tf_parser = subparsers.add_parser(
+        "tf", help="Get term frequency for a given document ID and term"
+    )
+    tf_parser.add_argument("doc_id", type=int, help="Document ID")
+    tf_parser.add_argument("term", type=str, help="Term to get frequency for")
+
     args = parser.parse_args()
 
     match args.command:
@@ -23,13 +29,12 @@ def main() -> None:
             print("Inverted index built successfully.")
         case "search":
             print("Searching for:", args.query)
-            try:
-                results = search_command(args.query)
-                for i, res in enumerate(results, 1):
-                    print(f"{i}. ({res['id']}) {res['title']}")
-            except FileNotFoundError:
-                print("File not found")
-
+            results = search_command(args.query)
+            for i, res in enumerate(results, 1):
+                print(f"{i}. ({res['id']}) {res['title']}")
+        case "tf":
+            tf = tf_command(args.doc_id, args.term)
+            print(f"Term frequency of '{args.term}' in document '{args.doc_id}': {tf}")
         case _:
             parser.print_help()
 
