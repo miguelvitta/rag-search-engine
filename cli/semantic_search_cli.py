fb1ebdb9de0fcb9207c25c42e43a04ff8fa11895
Miguel Vitta <miguelvittadev@gmail.com>
2026-04-06 10:36:47 -0300

    feat: implements a basic fixed chunking function

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -3,6 +3,7 @@
 import argparse
 
 from lib.semantic_search import (
+    chunk_text,
     embed_query_text,
     embed_text,
     semantic_search,
@@ -39,6 +40,14 @@ def main() -> None:
         "--limit", type=int, default=5, help="Number of results to return"
     )
 
+    chunk_parser = subparsers.add_parser(
+        "chunk", help="Split text into fixed-size chunks"
+    )
+    chunk_parser.add_argument("text", type=str, help="Text to chunk")
+    chunk_parser.add_argument(
+        "--chunk-size", type=int, default=200, help="Size of each chunk in words"
+    )
+
     args = parser.parse_args()
 
     match args.command:
@@ -52,6 +61,8 @@ def main() -> None:
             embed_query_text(args.query)
         case "search":
             semantic_search(args.query, args.limit)
+        case "chunk":
+            chunk_text(args.text, args.chunk_size)
         case _:
             parser.print_help()
 
